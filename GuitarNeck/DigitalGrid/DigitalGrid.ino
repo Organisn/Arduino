55aeb31c1b00fa3ed270ff8c9f563bba3ef327c3
Snidi <emanuele.snidero@gmail.com>
2026-01-19 15:05:20 +0100

    New code, circuit and scheme

--- GuitarNeck/DigitalGrid/DigitalGrid.ino
+++ GuitarNeck/DigitalGrid/DigitalGrid.ino
@@ -1,232 +1,223 @@
 // Note frequencies from public local library
 #include "pitches.h"
-// C, E, F, G, A pins status
-int cReading;
-int eReading;
-int fReading;
-int gReading;
-int aReading;
-// Memorize last buttons status: pressure, release or debounce
-int lastCButtonState = LOW;
-int lastEButtonState = LOW;
-int lastFButtonState = LOW;
-int lastGButtonState = LOW;
-int lastAButtonState = LOW;
+
+// Pins status...
+// from strings..
+uint8_t EReading;
+byte AReading;
+byte DReading;
+byte GReading;
+byte BReading;
+byte eReading;
+// and frets (starting from A from lower E string)
+byte fifthReading;
+byte sixthReading;
+byte seventhReading;
+byte eighthReading;
+// Memorize last buttons status (pressure, release or bounce)
+byte lastEButtonState = LOW;
+byte lastAButtonState = LOW;
+byte lastDButtonState = LOW;
+byte lastGButtonState = LOW;
+byte lastBButtonState = LOW;
+byte lasteButtonState = LOW;
+byte lastFifthButtonState = LOW;
+byte lastSixthButtonState = LOW;
+byte lastSeventhButtonState = LOW;
+byte lastEighthButtonState = LOW;
 // Memorize definitive buttons pressures or releases
-int cButtonState = LOW;
-int eButtonState = LOW;
-int fButtonState = LOW;
-int gButtonState = LOW;
-int aButtonState = LOW;
-// Memorize each button pressure or debounce time
-unsigned long lastCDebounceTime = 0;
-unsigned long lastEDebounceTime = 0;
-unsigned long lastFDebounceTime = 0;
-unsigned long lastGDebounceTime = 0;
-unsigned long lastADebounceTime = 0;
+byte EButtonState = LOW;
+byte AButtonState = LOW;
+byte DButtonState = LOW;
+byte GButtonState = LOW;
+byte BButtonState = LOW;
+byte eButtonState = LOW;
+byte fifthButtonState = LOW;
+byte sixthButtonState = LOW;
+byte seventhButtonState = LOW;
+byte eighthButtonState = LOW;
+// Memorize each button each pressure or bounce time
+unsigned long lastEBounceTime = 0;
+unsigned long lastABounceTime = 0;
+unsigned long lastDBounceTime = 0;
+unsigned long lastGBounceTime = 0;
+unsigned long lastBBounceTime = 0;
+unsigned long lasteBounceTime = 0;
+unsigned long lastFifthBounceTime = 0;
+unsigned long lastSixthBounceTime = 0;
+unsigned long lastSeventhBounceTime = 0;
+unsigned long lastEighthBounceTime = 0;
 // Debounce lapse
 unsigned long debounceDelay = 50;
-// Memorize time from program launch
+// Timestamp
 unsigned long lastTime;
-// Reset built-in func
-void(* resetFunc) (void) = 0;
-// Shift registers as 74HC595 turn out useful to save board IO pins (5, to be exact)
-// as when needed to turn on 7-segment (+ dot) displays
-// 74HC595 can store 8 bits at a time 
-// Each bit is dedicated to a specific display segment, as connected in the scheme:
-//     Q7
-//     _
-// Q2 |_| Q6
-//    Q1
-// Q3 |_| Q5 .Q0
-//     Q4
-// It is helpful to previously memorize each LEDs configuration,
-// keeping in mind pins representation into shifting byte:
-// (from most significative bit) Q7 Q6 Q5 Q4 Q3 Q2 Q1 Q0 (ex: 11111100 (=252) lights as 0)
-const uint8_t c = 0b10011100;
-const uint8_t e = 0b10011110;
-const uint8_t f = 0b10001110;
-const uint8_t g = 0b10111110;
-const uint8_t a = 0b11101110;
-const uint8_t r = 0b00001010; // 'r' for the board reset announce
-const uint8_t off = 0b00000000;
-// (Binary bytes can be memorized also as decimal integers)
-uint8_t playingNote = off;
+// Since multiple buttons can be simultaneously pressed,
+// every program cycle must be state which one is the highest-pitched
+unsigned int highestPressedKey = 0;
 
 void setup() {
-  pinMode(6, OUTPUT); // Buzzer
-  pinMode(12, INPUT); // C button
-  pinMode(11, INPUT); // E button
-  pinMode(10, INPUT); // F button
-  pinMode(9, INPUT); // G button
-  pinMode(8, INPUT); // A button
-  // 74HC595 pins
-  // DS: serial data output pin
-  pinMode(4, OUTPUT);
-  // ST_CP (storage register clock pin): data transmission enabler pin
-  pinMode(3, OUTPUT);
-  // SH_CP (shift register clock pin): serial data transmission clock pin
-  pinMode(2, OUTPUT);
-  // Buzzer and display check
-  /*
-  digitalWrite(3, LOW);
-  shiftOut(4, 2, MSBFIRST, a);
-  digitalWrite(3, HIGH);
-  tone(6, A4); // PWM required!
-  delay(500);
-  noTone(6);
-  */
+  Serial.begin(9600);
+  Serial.println("Starting playing..");
+  // Buttons grid
+  // Strings (from lower to higher)
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  // Frets (from lower to higher)
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  // Buzzer
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
   // Read current time
   lastTime = millis();
-  // Reset board after 2 minutes
-  if (lastTime > 120000) {
-    // Display an 'r' for 2s..
-    digitalWrite(3, LOW);
-    shiftOut(4, 2, MSBFIRST, r);
-    digitalWrite(3, HIGH);
-    delay(2000);
-    resetFunc();
-  }
-  // Check a button pin status variation only if buzzer's free to play or is already playing the note it is about
-  // (Otherwise it would be only a waste of time and energy, since buzzer can only play single notes per time...)
-  if (playingNote == off || playingNote == c) {
-    cReading = digitalRead(12);
-    if (cReading == HIGH) cButtonState = HIGH; // No need to wait debounce lapse to start making noise
-    // (we aren't affecting dangerous actuators pushing these buttons..)
-    // Emit tone and set buzzer as not available for other notes when button's pressed
-    if (cButtonState == HIGH) {
-      // Display note
-      digitalWrite(3, LOW); //ground ST_CP and hold low for as long as you are transmitting
-      shiftOut(4, 2, MSBFIRST, c); // Shift out data mode: Most Significant Bit FIRST
-      // shiftOut() can transmit only 8 bits per step
-      // (since we need not more than a byte to light up 8 LEDs, this time it is not required to call it two times..)
-      // Return the latch pin high to signal chip that it no longer needs to listen for information
-      // This will let 74HC595 to move data from shift to memory register (actually saving it..)
-      digitalWrite(3, HIGH);
-      tone(6, C5);
-      delay(20);
-      noTone(6);
-      playingNote = c;
-    }
-    if (cReading != lastCButtonState) lastCDebounceTime = lastTime; // Button pressure, release or debounce!
-    if ((lastTime - lastCDebounceTime) > debounceDelay) { // Button definitely pressed or released!
-      if (cReading != cButtonState) cButtonState = cReading;
-      // Free buzzer and display when button is released...
-      if (cButtonState == LOW) {
-        digitalWrite(3, LOW);
-        shiftOut(4, 2, MSBFIRST, 0);
-        digitalWrite(3, HIGH);
-        playingNote = off;
-      }
-    }
-    lastCButtonState = cReading;
-  }
-  // Do the same done with C button to others... 
-  if (playingNote == off || playingNote == e) {
-    eReading = digitalRead(11);
-    if (eReading == HIGH) eButtonState = HIGH;
-    // Emit tone and set buzzer as not available when button's pressed
-    if (eButtonState == HIGH) {
-      digitalWrite(3, LOW);
-      shiftOut(4, 2, MSBFIRST, e);
-      digitalWrite(3, HIGH);
-      tone(6, E5);
-      delay(15);
-      noTone(6);
-      playingNote = e;
-    }
-    if (eReading != lastEButtonState) lastEDebounceTime = lastTime; // Button pressure, release or debounce!
-    if ((lastTime - lastEDebounceTime) > debounceDelay) { // Button pressure or release!
-      if (eReading != eButtonState) eButtonState = eReading;
-      // Free buzzer and display when button is released...
-      if (eButtonState == LOW) {
-        digitalWrite(3, LOW);
-        shiftOut(4, 2, MSBFIRST, 0);
-        digitalWrite(3, HIGH);
-        playingNote = off;
-      }
-    }
-    lastEButtonState = eReading;
-  }
-  if (playingNote == off || playingNote == f) {
-    fReading = digitalRead(10);
-    if (fReading == HIGH) fButtonState = HIGH;
-    // Emit tone and set buzzer as not available when button's pressed
-    if (fButtonState == HIGH) {
-      digitalWrite(3, LOW);
-      shiftOut(4, 2, MSBFIRST, f);
-      digitalWrite(3, HIGH);
-      tone(6, F5);
-      delay(10);
-      noTone(6);
-      playingNote = f;
-    }
-    if (fReading != lastFButtonState) lastFDebounceTime = lastTime; // Button pressure, release or debounce!
-    if ((lastTime - lastFDebounceTime) > debounceDelay) { // Button pressure or release!
-      if (fReading != fButtonState) fButtonState = fReading;
-      // Free buzzer and display when button is released...
-      if (fButtonState == LOW) {
-        digitalWrite(3, LOW);
-        shiftOut(4, 2, MSBFIRST, 0);
-        digitalWrite(3, HIGH);
-        playingNote = off;
-      }
-    }
-    lastFButtonState = fReading;
-  }
-  if (playingNote == off || playingNote == g) {
-    gReading = digitalRead(9);
-    if (gReading == HIGH) gButtonState = HIGH;
-    // Emit tone and set buzzer as not available when button's pressed
-    if (gButtonState == HIGH) {
-      digitalWrite(3, LOW);
-      shiftOut(4, 2, MSBFIRST, g);
-      digitalWrite(3, HIGH);
-      tone(6, G5);
-      delay(25);
-      noTone(6);
-      playingNote = g;
-    }
-    if (gReading != lastGButtonState) lastGDebounceTime = lastTime; // Button pressure, release or debounce!
-    if ((lastTime - lastGDebounceTime) > debounceDelay) { // Button pressure or release!
-      if (gReading != gButtonState) gButtonState = gReading;
-      // Free buzzer and display when button is released...
-      if (gButtonState == LOW) {
-        digitalWrite(3, LOW);
-        shiftOut(4, 2, MSBFIRST, 0);
-        digitalWrite(3, HIGH);
-        playingNote = off;
-      }
-    }
-    lastGButtonState = gReading;
-  }
-  if (playingNote == off || playingNote == a) {
-    aReading = digitalRead(8);
-    if (aReading == HIGH) aButtonState = HIGH;
-    // Emit tone and set buzzer as not available when button's pressed
-    if (aButtonState == HIGH) {
-      digitalWrite(3, LOW);
-      shiftOut(4, 2, MSBFIRST, a);
-      digitalWrite(3, HIGH);
-      tone(6, A5);
-      delay(25);
-      noTone(6);
-      playingNote = a;
-    }
-    if (aReading != lastAButtonState) lastADebounceTime = lastTime; // Button pressure, release or debounce!
-    if ((lastTime - lastADebounceTime) > debounceDelay) { // Button pressure or release!
-      if (aReading != aButtonState) aButtonState = aReading;
-      // Free buzzer and display when button is released...
-      if (aButtonState == LOW) {
-        digitalWrite(3, LOW);
-        shiftOut(4, 2, MSBFIRST, 0);
-        digitalWrite(3, HIGH);
-        playingNote = off;
-      }
-    }
-    lastAButtonState = aReading;
-  }
+  // Check each button pin status variation to say which buttons
+  // are actually pressed at each moment...
+  // E string
+  EReading = digitalRead(0);
+  // No need to wait debounce lapse to start making noise
+  // (we aren't affecting dangerous actuators pushing these buttons..)
+  if (EReading == HIGH) EButtonState = HIGH;
+  // But we must wait debounce lapse to say if button is definitely pressed or released..
+  if (EReading != lastEButtonState) lastEBounceTime = lastTime; // Button pressure, release or debounce!
+  if ((lastTime - lastEBounceTime) > debounceDelay) // Button definitely pressed or released!
+  	EButtonState = EReading;
+  lastEButtonState = EReading;
+  // A string
+  AReading = digitalRead(1);
+  if (AReading == HIGH) AButtonState = HIGH;
+  if (AReading != lastAButtonState) lastABounceTime = lastTime;
+  if ((lastTime - lastABounceTime) > debounceDelay)
+  	AButtonState = AReading;
+  lastAButtonState = AReading;
+  // D string
+  DReading = digitalRead(2);
+  if (DReading == HIGH) DButtonState = HIGH;
+  if (DReading != lastDButtonState) lastDBounceTime = lastTime;
+  if ((lastTime - lastDBounceTime) > debounceDelay)
+  	DButtonState = DReading;
+  lastDButtonState = DReading;
+  // G string
+  GReading = digitalRead(3);
+  if (GReading == HIGH) GButtonState = HIGH;
+  if (GReading != lastGButtonState) lastGBounceTime = lastTime;
+  if ((lastTime - lastGBounceTime) > debounceDelay)
+  	GButtonState = GReading;
+  lastGButtonState = GReading;
+  // B string
+  BReading = digitalRead(4);
+  if (BReading == HIGH) BButtonState = HIGH;
+  if (BReading != lastBButtonState) lastBBounceTime = lastTime;
+  if ((lastTime - lastBBounceTime) > debounceDelay)
+  	BButtonState = BReading;
+  lastBButtonState = BReading;
+  // e string
+  eReading = digitalRead(5);
+  if (eReading == HIGH) eButtonState = HIGH;
+  if (eReading != lasteButtonState) lasteBounceTime = lastTime;
+  if ((lastTime - lasteBounceTime) > debounceDelay)
+  	eButtonState = eReading;
+  lasteButtonState = eReading;
+  // Fifth fret
+  fifthReading = digitalRead(6);
+  if (fifthReading == HIGH) fifthButtonState = HIGH;
+  if (fifthReading != lastFifthButtonState) lastFifthBounceTime = lastTime;
+  if ((lastTime - lastFifthBounceTime) > debounceDelay)
+  	fifthButtonState = fifthReading;
+  lastFifthButtonState = fifthReading;
+  // Sixth fret
+  sixthReading = digitalRead(7);
+  if (sixthReading == HIGH) sixthButtonState = HIGH;
+  if (sixthReading != lastSixthButtonState) lastSixthBounceTime = lastTime;
+  if ((lastTime - lastSixthBounceTime) > debounceDelay)
+  	sixthButtonState = sixthReading;
+  lastSixthButtonState = sixthReading;
+  // Seventh fret
+  seventhReading = digitalRead(8);
+  if (seventhReading == HIGH) seventhButtonState = HIGH;
+  if (seventhReading != lastSeventhButtonState) lastSeventhBounceTime = lastTime;
+  if ((lastTime - lastSeventhBounceTime) > debounceDelay)
+  	seventhButtonState = seventhReading;
+  lastSeventhButtonState = seventhReading;
+  // Eighth fret
+  eighthReading = digitalRead(9);
+  if (eighthReading == HIGH) eighthButtonState = HIGH;
+  if (eighthReading != lastEighthButtonState) lastEighthBounceTime = lastTime;
+  if ((lastTime - lastEighthBounceTime) > debounceDelay)
+  	eighthButtonState = eighthReading;
+  lastEighthButtonState = eighthReading;
+  
+  // Checking each string and fret from lower to higher let us update
+  // the highest key pressed when multiple keys are simultaneously pressed
+  // (and oc when a single higher key is pressed too)
+  if (EButtonState == HIGH && fifthButtonState == HIGH) // check if brackets necessary
+  	highestPressedKey = 440;
+  if (EButtonState == HIGH && sixthButtonState == HIGH)
+    highestPressedKey = 466;
+  if (EButtonState == HIGH && seventhButtonState == HIGH)
+    highestPressedKey = 494;
+  if (EButtonState == HIGH && eighthButtonState == HIGH)
+    highestPressedKey = 523;
+  if (AButtonState == HIGH && fifthButtonState == HIGH)
+    highestPressedKey = 554;
+  if (AButtonState == HIGH && sixthButtonState == HIGH)
+    highestPressedKey = 587;
+  if (AButtonState == HIGH && seventhButtonState == HIGH)
+    highestPressedKey = 622;
+  if (AButtonState == HIGH && eighthButtonState == HIGH)
+    highestPressedKey = 659;
+  if (DButtonState == HIGH && fifthButtonState == HIGH)
+    highestPressedKey = 698;
+  if (DButtonState == HIGH && sixthButtonState == HIGH)
+    highestPressedKey = 740;
+  if (DButtonState == HIGH && seventhButtonState == HIGH)
+    highestPressedKey = 784;
+  if (DButtonState == HIGH && eighthButtonState == HIGH)
+    highestPressedKey = 831;
+  if (GButtonState == HIGH && fifthButtonState == HIGH)
+    highestPressedKey = 880;
+  if (GButtonState == HIGH && sixthButtonState == HIGH)
+    highestPressedKey = 932;
+  if (GButtonState == HIGH && seventhButtonState == HIGH)
+    highestPressedKey = 988;
+  if (GButtonState == HIGH && eighthButtonState == HIGH)
+    highestPressedKey = 1047;
+  if (BButtonState == HIGH && fifthButtonState == HIGH)
+    highestPressedKey = 1109;
+  if (BButtonState == HIGH && sixthButtonState == HIGH)
+    highestPressedKey = 1175;
+  if (BButtonState == HIGH && seventhButtonState == HIGH)
+    highestPressedKey = 1245;
+  if (BButtonState == HIGH && eighthButtonState == HIGH)
+    highestPressedKey = 1319;
+  if (eButtonState == HIGH && fifthButtonState == HIGH)
+    highestPressedKey = 1397;
+  if (eButtonState == HIGH && sixthButtonState == HIGH)
+    highestPressedKey = 1480;
+  if (eButtonState == HIGH && seventhButtonState == HIGH)
+    highestPressedKey = 1568;
+  if (eButtonState == HIGH && eighthButtonState == HIGH)
+    highestPressedKey = 1661;
+          
+  // Stop sound emission when no key pressed..
+  if ((EButtonState == LOW) && 
+      (AButtonState == LOW) && 
+      (DButtonState == LOW) && 
+      (GButtonState == LOW) && 
+      (BButtonState == LOW) && 
+      (eButtonState == LOW) &&
+      (fifthButtonState == LOW) &&
+      (sixthButtonState == LOW) &&
+      (seventhButtonState == LOW) &&
+      (eighthButtonState == LOW))
+    // If noTone() isn't called, tone() will eventually continue to work undisturbed
+    noTone(10);
+  else tone(10, highestPressedKey);
 }
