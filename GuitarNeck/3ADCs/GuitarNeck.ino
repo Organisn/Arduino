e1fd1b4e6a6e2351c57c83f9b529367f6531ca93
Snidi <emanuele.snidero@gmail.com>
2026-02-20 05:40:18 +0100

    Updated resistors sums; re-enabled all 3 analog readings

--- GuitarNeck/3ADCs/GuitarNeck.ino
+++ GuitarNeck/3ADCs/GuitarNeck.ino
@@ -17,7 +17,7 @@ void setup() {
 }
 
 void loop() {
-  for (byte i = 0; i < 1; i = i + 1) { // !!! i < 3 !!!
+  for (byte i = 0; i < 3; i = i + 1) {
     resistances[i] = analogRead(i); // IMPLEMENT BUTTON DEBOUNCE? AND SIGNAL STABILIZATION?
     // Each button is put in series with a single unique resistance
     // and in parallel with all that others sharing its analog pin, 
@@ -54,28 +54,158 @@ void loop() {
         switch (resistances[0]) {
           // The lower the key, the lower the resistance, the higher the voltage read...
           // MUST UPDATE THIS SWITCH CASE WITH ACTUAL RESISTOR VALUES AND THEIR CORRESPONDING READINGS (AND THEIR COMBOS)!!
-          case 255: // 1
+          case 254: // 1 
             highestPressedKey = A4;
             break;
-          case 254: // 2
-          case 253: // 3
+          case 253: // 2
+          case 252: // 3 (if 1 and 2 ohm parallel resistors are crossed by current simultaneously, their resultant resistance is 0,66 ohm, which is read as 254 instead of 252..)
             // When multiple keys are pressed, their resistances are multiplied and divided by their sum...
             // As said, only the higher note is played..
             highestPressedKey = AS4; // Highest pressed key is updated only if higher keys are actually pressed..
             break;
-          case 252: // 4
-          case 251: // 5
-          case 250: // 6
-          case 249: // 7
+          case 251: // 4
+          case 250: // 5
+          case 249: // 6
+          case 248: // 7
             highestPressedKey = B4;
             break;
-          case 123: // 4
-          case 122: // 5
-          case 121: // 6
-          case 120: // 7
+          case 247: // 8
+          case 246: // 9
+          case 245: // 10
+          case 244: // 11
+          case 243: // 12
+          case 242: // 13
+          case 241: // 14
+          case 240: // 15
             highestPressedKey = C5;
             break;
-          case 119: // 8
+          case 239: // 16
+          case 238:
+          case 237:
+          case 236:
+          case 235:
+          case 234:
+          case 233:
+          case 232:
+          case 231:
+          case 230:
+          case 229:
+          case 228:
+          case 227:
+          case 226:
+          case 225:
+          case 224:
+            highestPressedKey = D5;
+            break;
+          case 223: // 32
+          case 222:
+          case 221:
+          case 220:
+          case 219:
+          case 218:
+          case 217:
+          case 216:
+          case 215:
+          case 214:
+          case 213:
+          case 212:
+          case 211:
+          case 210:
+          case 209:
+          case 208:
+          case 207:
+          case 206:
+          case 205:
+          case 204:
+          case 203:
+          case 202:
+          case 201:
+          case 200:
+          case 199:
+          case 198:
+          case 197:
+          case 196:
+          case 195:
+          case 194:
+          case 193:
+          case 192:
+            highestPressedKey = DS5;
+            break;
+          case 191: // 64
+          case 190:
+          case 189:
+          case 188:
+          case 187:
+          case 186:
+          case 185:
+          case 184:
+          case 183:
+          case 182:
+          case 181:
+          case 180:
+          case 179:
+          case 178:
+          case 177:
+          case 176:
+          case 175:
+          case 174:
+          case 173:
+          case 172:
+          case 171:
+          case 170:
+          case 169:
+          case 168:
+          case 167:
+          case 166:
+          case 165:
+          case 164:
+          case 163:
+          case 162:
+          case 161:
+          case 160:
+          case 159:
+          case 158:
+          case 157:
+          case 156:
+          case 155:
+          case 154:
+          case 153:
+          case 152:
+          case 151:
+          case 150:
+          case 149:
+          case 148:
+          case 147:
+          case 146:
+          case 145:
+          case 144:
+          case 143:
+          case 142:
+          case 141:
+          case 140:
+          case 139:
+          case 138:
+          case 137:
+          case 136:
+          case 135:
+          case 134:
+          case 133:
+          case 132:
+          case 131:
+          case 130:
+          case 129:
+          case 128:
+            highestPressedKey = E5;
+            break;
+          case 127: // 128
+          case 126:
+          case 125:
+          case 124:
+          case 123:
+          case 122:
+          case 121:
+          case 120:
+          case 119:
           case 118:
           case 117:
           case 116:
@@ -83,9 +213,7 @@ void loop() {
           case 114:
           case 113:
           case 112:
-            highestPressedKey = D5;
-            break;
-          case 111: // 16
+          case 111:
           case 110:
           case 109:
           case 108:
@@ -101,9 +229,7 @@ void loop() {
           case 98:
           case 97:
           case 96:
-            highestPressedKey = DS5;
-            break;
-          case 95: // 32
+          case 95:
           case 94:
           case 93:
           case 92:
@@ -135,9 +261,7 @@ void loop() {
           case 66:
           case 65:
           case 64:
-            highestPressedKey = E5;
-            break;
-          case 63: // 64
+          case 63:
           case 62:
           case 61:
           case 60:
@@ -200,6 +324,7 @@ void loop() {
           case 3:
           case 2:
           case 1:
+          // case 0:
             highestPressedKey = F5;
             break;
         }
@@ -207,22 +332,157 @@ void loop() {
       case 1:
         Serial.println("From middle strings: " + String(resistances[1]));
         switch (resistances[1]) {
-          case 127:
+          case 254: // 1 
             highestPressedKey = G5;
             break;
-          case 126:
-            highestPressedKey = GS5;
+          case 253: // 2
+          case 252: // 3 (if 1 and 2 ohm parallel resistors are crossed by current simultaneously, their resultant resistance is 0,66 ohm, which is read as 254 instead of 252..)
+            // When multiple keys are pressed, their resistances are multiplied and divided by their sum...
+            // As said, only the higher note is played..
+            highestPressedKey = GS5; // Highest pressed key is updated only if higher keys are actually pressed..
             break;
-          case 125:
-          case 124:
+          case 251: // 4
+          case 250: // 5
+          case 249: // 6
+          case 248: // 7
             highestPressedKey = A5;
             break;
+          case 247: // 8
+          case 246: // 9
+          case 245: // 10
+          case 244: // 11
+          case 243: // 12
+          case 242: // 13
+          case 241: // 14
+          case 240: // 15
+            highestPressedKey = AS5;
+            break;
+          case 239: // 16
+          case 238:
+          case 237:
+          case 236:
+          case 235:
+          case 234:
+          case 233:
+          case 232:
+          case 231:
+          case 230:
+          case 229:
+          case 228:
+          case 227:
+          case 226:
+          case 225:
+          case 224:
+            highestPressedKey = C6;
+            break;
+          case 223: // 32
+          case 222:
+          case 221:
+          case 220:
+          case 219:
+          case 218:
+          case 217:
+          case 216:
+          case 215:
+          case 214:
+          case 213:
+          case 212:
+          case 211:
+          case 210:
+          case 209:
+          case 208:
+          case 207:
+          case 206:
+          case 205:
+          case 204:
+          case 203:
+          case 202:
+          case 201:
+          case 200:
+          case 199:
+          case 198:
+          case 197:
+          case 196:
+          case 195:
+          case 194:
+          case 193:
+          case 192:
+            highestPressedKey = CS6;
+            break;
+          case 191: // 64
+          case 190:
+          case 189:
+          case 188:
+          case 187:
+          case 186:
+          case 185:
+          case 184:
+          case 183:
+          case 182:
+          case 181:
+          case 180:
+          case 179:
+          case 178:
+          case 177:
+          case 176:
+          case 175:
+          case 174:
+          case 173:
+          case 172:
+          case 171:
+          case 170:
+          case 169:
+          case 168:
+          case 167:
+          case 166:
+          case 165:
+          case 164:
+          case 163:
+          case 162:
+          case 161:
+          case 160:
+          case 159:
+          case 158:
+          case 157:
+          case 156:
+          case 155:
+          case 154:
+          case 153:
+          case 152:
+          case 151:
+          case 150:
+          case 149:
+          case 148:
+          case 147:
+          case 146:
+          case 145:
+          case 144:
+          case 143:
+          case 142:
+          case 141:
+          case 140:
+          case 139:
+          case 138:
+          case 137:
+          case 136:
+          case 135:
+          case 134:
+          case 133:
+          case 132:
+          case 131:
+          case 130:
+          case 129:
+          case 128:
+            highestPressedKey = D6;
+            break;
+          case 127: // 128
+          case 126:
+          case 125:
+          case 124:
           case 123:
           case 122:
           case 121:
           case 120:
-            highestPressedKey = AS5;
-            break;
           case 119:
           case 118:
           case 117:
@@ -231,8 +491,6 @@ void loop() {
           case 114:
           case 113:
           case 112:
-            highestPressedKey = C6;
-            break;
           case 111:
           case 110:
           case 109:
@@ -249,8 +507,6 @@ void loop() {
           case 98:
           case 97:
           case 96:
-            highestPressedKey = CS6;
-            break;
           case 95:
           case 94:
           case 93:
@@ -283,8 +539,6 @@ void loop() {
           case 66:
           case 65:
           case 64:
-            highestPressedKey = D6;
-            break;
           case 63:
           case 62:
           case 61:
@@ -348,6 +602,7 @@ void loop() {
           case 3:
           case 2:
           case 1:
+          // case 0:
             highestPressedKey = DS6;
             break;
         }
@@ -355,22 +610,157 @@ void loop() {
       case 2:
         Serial.println("From higher strings: " + String(resistances[2]));
         switch (resistances[2]) {
-          case 127:
+          case 254: // 1 
             highestPressedKey = E6;
             break;
-          case 126:
-            highestPressedKey = F6;
+          case 253: // 2
+          case 252: // 3 (if 1 and 2 ohm parallel resistors are crossed by current simultaneously, their resultant resistance is 0,66 ohm, which is read as 254 instead of 252..)
+            // When multiple keys are pressed, their resistances are multiplied and divided by their sum...
+            // As said, only the higher note is played..
+            highestPressedKey = F6; // Highest pressed key is updated only if higher keys are actually pressed..
             break;
-          case 125:
-          case 124:
+          case 251: // 4
+          case 250: // 5
+          case 249: // 6
+          case 248: // 7
             highestPressedKey = FS6;
             break;
+          case 247: // 8
+          case 246: // 9
+          case 245: // 10
+          case 244: // 11
+          case 243: // 12
+          case 242: // 13
+          case 241: // 14
+          case 240: // 15
+            highestPressedKey = G6;
+            break;
+          case 239: // 16
+          case 238:
+          case 237:
+          case 236:
+          case 235:
+          case 234:
+          case 233:
+          case 232:
+          case 231:
+          case 230:
+          case 229:
+          case 228:
+          case 227:
+          case 226:
+          case 225:
+          case 224:
+            highestPressedKey = A6;
+            break;
+          case 223: // 32
+          case 222:
+          case 221:
+          case 220:
+          case 219:
+          case 218:
+          case 217:
+          case 216:
+          case 215:
+          case 214:
+          case 213:
+          case 212:
+          case 211:
+          case 210:
+          case 209:
+          case 208:
+          case 207:
+          case 206:
+          case 205:
+          case 204:
+          case 203:
+          case 202:
+          case 201:
+          case 200:
+          case 199:
+          case 198:
+          case 197:
+          case 196:
+          case 195:
+          case 194:
+          case 193:
+          case 192:
+            highestPressedKey = AS6;
+            break;
+          case 191: // 64
+          case 190:
+          case 189:
+          case 188:
+          case 187:
+          case 186:
+          case 185:
+          case 184:
+          case 183:
+          case 182:
+          case 181:
+          case 180:
+          case 179:
+          case 178:
+          case 177:
+          case 176:
+          case 175:
+          case 174:
+          case 173:
+          case 172:
+          case 171:
+          case 170:
+          case 169:
+          case 168:
+          case 167:
+          case 166:
+          case 165:
+          case 164:
+          case 163:
+          case 162:
+          case 161:
+          case 160:
+          case 159:
+          case 158:
+          case 157:
+          case 156:
+          case 155:
+          case 154:
+          case 153:
+          case 152:
+          case 151:
+          case 150:
+          case 149:
+          case 148:
+          case 147:
+          case 146:
+          case 145:
+          case 144:
+          case 143:
+          case 142:
+          case 141:
+          case 140:
+          case 139:
+          case 138:
+          case 137:
+          case 136:
+          case 135:
+          case 134:
+          case 133:
+          case 132:
+          case 131:
+          case 130:
+          case 129:
+          case 128:
+            highestPressedKey = B6;
+            break;
+          case 127: // 128
+          case 126:
+          case 125:
+          case 124:
           case 123:
           case 122:
           case 121:
           case 120:
-            highestPressedKey = G6;
-            break;
           case 119:
           case 118:
           case 117:
@@ -379,8 +769,6 @@ void loop() {
           case 114:
           case 113:
           case 112:
-            highestPressedKey = A6;
-            break;
           case 111:
           case 110:
           case 109:
@@ -397,8 +785,6 @@ void loop() {
           case 98:
           case 97:
           case 96:
-            highestPressedKey = AS6;
-            break;
           case 95:
           case 94:
           case 93:
@@ -431,8 +817,6 @@ void loop() {
           case 66:
           case 65:
           case 64:
-            highestPressedKey = B6;
-            break;
           case 63:
           case 62:
           case 61:
@@ -496,6 +880,7 @@ void loop() {
           case 3:
           case 2:
           case 1:
+          // case 0:
             highestPressedKey = C7;
             break;
         }
